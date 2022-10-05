cd4deb43ab4602cdd76373dbc7776329d2dda9c7
Matheus <matheus110903@gmail.com>
2022-10-05 18:09:30 -0300

    alterado estrutura para hooks

--- React-App-BASICO/HooksMigration/src/templates/home/index.jsx
+++ React-App-BASICO/HooksMigration/src/templates/home/index.jsx
@@ -1,77 +1,64 @@
-// import logo from "./logo.svg";
 import "./Home.css";
-import { Component } from "react";
 
 import { loadPosts } from "../../utils/load-posts";
 import { Posts } from "../../components/posts";
 import { MoreBtn } from "../../components/Button";
 import { TextInput } from "../../components/TextInput";
+import { useCallback, useEffect, useState } from "react";
 
-export class Home extends Component {
-  state = {
-    posts: [],
-    allPosts: [],
-    page: 0,
-    postsPerPage: 11,
-    searchValue: ''
-  };
 
-  async componentDidMount() {
-    await this.loadPosts();
-  }
-  
-  // fetch(`https://jsonplaceholder.typicode.com/posts`).then(res => res.json()).then(posts => this.setState({posts}))
+export const Home = () => {
+  const [page, setPage] = useState(0);
+  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+  // const [postsPerPage, setPostsPerPage] = useState(11);
+  const [postsPerPage] = useState(11);
 
-  loadPosts = async () => {
-    const { page, postsPerPage} = this.state;
+  const noMorePosts = page + postsPerPage >= allPosts.length;
+  const filteredPosts = !!searchValue ? allPosts.filter(post => post.title.toLowerCase().includes(searchValue.toLowerCase())) : posts;
+  // console.log(filteredPosts);
+
+  const handleLoadPosts = useCallback(async (page, postsPerPage) => {
     const postsAndPhotos = await loadPosts();
-    this.setState({ 
-      posts: postsAndPhotos.slice(page, postsPerPage),
-      allPosts: postsAndPhotos,
-    
-    });
-  }
 
-  loadMorePosts = () => {
-    const { page, posts, allPosts, postsPerPage } = this.state;
+    setPosts(postsAndPhotos.slice(page, postsPerPage))
+    setAllPosts(postsAndPhotos);
+  }, [])
+
+  useEffect(() => {console.log(new Date().toLocaleString('pt-br'), new Date()); handleLoadPosts(0, postsPerPage)}, [handleLoadPosts, postsPerPage]);
+
+  const loadMorePosts = () => {
     const nextPage = page + postsPerPage;
     const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
     posts.push(...nextPosts);
-    this.setState({ posts, page: nextPage });
+    setPosts(posts);
+    setPage(nextPage);
 
     // console.log(`PAGE: ${page}, POSTS:`, posts, `ALLPOSTS:`, allPosts,`POSTSPERPAGE: ${postsPerPage},NEXTPAGE: ${nextPage}, NEXTOFNEXTPAGE:${nextPage + postsPerPage}`);
     // console.log("load more posts called...");
   }
 
-  handleChange = (e) => {
+  const handleChange = (e) => {
     const {value} = e.target;
-    this.setState({searchValue: value});
+    setSearchValue(value);
   }
 
+  return (
+    <section className="section-container">
+      <div className="in">
+        {!!searchValue && (<h1>Search: <br/> {searchValue}</h1>)} 
+        <TextInput searchValue={searchValue} handleChange={handleChange} />
+      </div>
 
-  render() {
-    const { page, posts, allPosts, searchValue, postsPerPage  } = this.state;
-    const noMorePosts = page + postsPerPage >= allPosts.length;
-    const filteredPosts = !!searchValue ? allPosts.filter(post => post.title.toLowerCase().includes(searchValue.toLowerCase())) : posts;
-    // console.log(filteredPosts);
-
-    return (
-      <section className="section-container">
-        <div className="in">
-          {!!searchValue && (<h1>Search: <br/> {searchValue}</h1>)} 
-          <TextInput searchValue={searchValue} handleChange={this.handleChange} />
-        </div>
-
-        <div className="search-container">
-          {filteredPosts.length > 0 && (<Posts posts={filteredPosts} />)}
-          {filteredPosts.length === 0 && (<h6>Search not found.</h6>)}
-        </div>
-        
-        <div className="more-container">
-          {!searchValue && (<MoreBtn isD={noMorePosts} text="Load More" oC={this.loadMorePosts}/>)}
-        </div>
-      </section>
-    );
-  }
+      <div className="search-container">
+        {filteredPosts.length > 0 && (<Posts posts={filteredPosts} />)}
+        {filteredPosts.length === 0 && (<h6>Search not found.</h6>)}
+      </div>
+      
+      <div className="more-container">
+        {!searchValue && (<MoreBtn isD={noMorePosts} text="Load More" oC={loadMorePosts}/>)}
+      </div>
+    </section>
+  );
 }
-
