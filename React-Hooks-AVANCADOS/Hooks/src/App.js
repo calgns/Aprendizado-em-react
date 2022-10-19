456c3d81582056fad5a53918b4413c2144a87ff8
Matheus <matheus110903@gmail.com>
2022-10-19 15:27:53 -0300

    entendido memo e callback. .051

--- React-Hooks-AVANCADOS/Hooks/src/App.js
+++ React-Hooks-AVANCADOS/Hooks/src/App.js
@@ -1,30 +1,50 @@
 import P from 'prop-types';
 import './App.css';
-import React, { useState, useCallback } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 
-const Button = React.memo(function Button({ incrementButton }) {
-  console.log('Filho, renderizou');
-  return <button onClick={() => incrementButton(100)}>+</button>;
-});
+const Post = ({ post }) => {
+  console.log('Fi renderizo!');
+  return (
+    <div key={post.id} className="post">
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
+};
 
-Button.propTypes = {
-  incrementButton: P.func,
+Post.propTypes = {
+  post: P.shape({
+    id: P.number,
+    title: P.string,
+    body: P.string,
+  }),
 };
 
 function App() {
-  const [counter, setCounter] = useState(0);
+  const [posts, setPosts] = useState([]);
+  const [value, setValue] = useState('');
 
-  const incrementCounter = useCallback((num) => {
-    setCounter((c) => c + num);
+  console.log(' Renderizou pa');
+
+  // componentDidMount
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((r) => r.json())
+        .then((r) => setPosts(r));
+    }, 5000);
   }, []);
 
-  console.log('Pai, renderizou');
-
   return (
-    <div className="App">
-      <p>Teste 3</p>
-      <h1>C1: {counter}</h1>
-      <Button incrementButton={incrementCounter} />
+    <div className="App-header">
+      <p>
+        <input type="search" value={value} onChange={(e) => setValue(e.target.value)} />
+      </p>
+      {useMemo(() => {
+        return posts.length > 0 && posts.map((post) => <Post key={post.id} post={post} />);
+      }, [posts])}
+
+      {posts.length <= 0 && <div className="spinner"></div>}
     </div>
   );
 }
