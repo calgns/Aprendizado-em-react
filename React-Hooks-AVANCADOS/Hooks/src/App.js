f3a0277c9f37cdfcdbb5c8678f6aa58c9c0abd9d
Matheus <matheus110903@gmail.com>
2022-10-21 18:28:59 -0300

    entendendo useContext + useReducer. .056

--- React-Hooks-AVANCADOS/Hooks/src/App.js
+++ React-Hooks-AVANCADOS/Hooks/src/App.js
@@ -1,40 +1,68 @@
-import { useReducer } from 'react';
+import P from 'prop-types';
+import { createContext, useContext, useReducer, useRef } from 'react';
 import './App.css';
 
-const globalState = {
+// actions.js
+export const actions = {
+  CHANGE_TITLE: 'CHANGE_TITLE',
+};
+
+// data.js
+export const globalState = {
   title: 'O título que contexto',
   body: 'O body do contexto',
   counter: 0,
 };
 
-const reducer = (state, action) => {
+// reducer.js
+export const reducer = (state, action) => {
   switch (action.type) {
-    case 'muda': {
-      console.log('chamou muda', action.payload);
+    case actions.CHANGE_TITLE: {
+      console.log('Mudar título');
       return { ...state, title: action.payload };
     }
-    case 'inverter': {
-      console.log('chamou inverter');
-      const { title } = state;
-      return { ...state, title: title.split('').reverse().join('') };
-    }
   }
-  console.log('NENHUMA ACTION ENCONTRADA!');
+
   return { ...state };
 };
 
-function App() {
+// AppContext.jsx
+export const Context = createContext();
+export const AppContext = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, globalState);
-  const { counter, title, body } = state;
+
+  const changeTitle = (payload) => {
+    dispatch({ type: actions.CHANGE_TITLE, payload });
+  };
+
+  return <Context.Provider value={{ state, changeTitle }}>{children}</Context.Provider>;
+};
+
+AppContext.propTypes = {
+  children: P.node,
+};
+
+// H1/index.jsx
+export const H1 = () => {
+  const context = useContext(Context);
+  const inputRef = useRef();
+
   return (
-    <div>
-      <h1>
-        {title} {counter} {body}
-      </h1>
-      <button onClick={() => dispatch({ type: 'muda', payload: new Date().toLocaleDateString('pt-BR') })}>Mude!</button>
-      <button onClick={() => dispatch({ type: 'inverter' })}>Inverta!</button>
-      <button onClick={() => dispatch({ type: 'none' })}>nrh</button>
-    </div>
+    <>
+      <h1 onClick={() => context.changeTitle(inputRef.current.value)}>{context.state.title}</h1>
+      <input type="text" ref={inputRef} />
+    </>
+  );
+};
+
+// App.jsx
+function App() {
+  return (
+    <AppContext>
+      <div>
+        <H1 />
+      </div>
+    </AppContext>
   );
 }
 
